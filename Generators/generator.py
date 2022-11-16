d4fbd6131dedeba7c057725d081b245d26cf227a
Tạ Quang Khôi <khoi0941@gmail.com>
2022-11-16 15:42:31 +0700

    chore: move generator.py to Generators

--- Generators/generator.py
+++ Generators/generator.py
@@ -43,3 +43,29 @@ for i in range(m):
     for j in range(n):
         print(matrix[i][j], end=" ")
     print()
+
+# Find max value in matrix
+max = matrix[0][0]
+for i in range(1, len(matrix)):
+    for j in range(1, len(matrix[i])):
+        if max < matrix[i][j]:
+            max = matrix[i][j]
+
+print("Max value in matrix: ", max)
+
+# Find min value in matrix
+min = matrix[0][0]
+for i in range(1, len(matrix)):
+    for j in range(1, len(matrix[i])):
+        if min > matrix[i][j]:
+            min = matrix[i][j]
+
+print("Min value in matrix: ", min)
+
+# Find sum of matrix
+sum = 0
+for i in range(len(matrix)):
+    for j in range(len(matrix[i])):
+        sum += matrix[i][j]
+
+print("Sum of matrix: ", sum)
