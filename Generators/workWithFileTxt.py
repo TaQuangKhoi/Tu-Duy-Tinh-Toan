9bcf3b7cc7e6e41bd2b82bc47058682b2792d95e
Tạ Quang Khôi <khoi0941@gmail.com>
2022-11-23 14:00:27 +0700

    update workWithFileTxt.py

--- Generators/workWithFileTxt.py
+++ Generators/workWithFileTxt.py
@@ -1,3 +1,34 @@
 import os
-os.chdir('C:\\My_Repos\\GitHub\\TaQuangKhoi\\Tu-Duy-Tinh-Toan-1')
 
+os.chdir('C:\\My_Repos\\GitHub\\TaQuangKhoi\\Tu-Duy-Tinh-Toan-1')
+path = "text.txt"
+
+x = []
+
+f = open(path , "r")
+n = int(f.readline())
+line = f.readline()
+line = line.split()
+
+for i in line:
+    x.append(float(i))
+
+f.close()
+
+print("Tổng ptử", n)
+print("Mảng là: ", x)
+
+# max of x
+max = x[0]
+for i in range(1, len(x)):
+    if max < x[i]:
+        max = x[i]
+
+print("Max value in x: ", max)
+
+# total of x
+total = 0
+for i in range(len(x)):
+    total += x[i]
+
+print("Total of x: ", total)
