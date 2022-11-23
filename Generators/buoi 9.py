109056f863d729282d2c7c8a139e593f3a890619
Tạ Quang Khôi <khoi0941@gmail.com>
2022-11-23 13:22:30 +0700

    update 'buoi 9.py'

--- Generators/buoi 9.py
+++ Generators/buoi 9.py
@@ -1,7 +1,9 @@
-# Cho ds M = [[1], [1, 2, 4, 6, 8], [1, 2, 10], [1, 2, 3, 4]]. Hãy in ra list dài nhất trong danh sách này.
+# Cho ds M = [[1], [1, 2, 4, 6, 8], [1, 2, 10], [1, 2, 3, 4]].
+# Hãy in ra list dài nhất trong danh sách này.
 M = [[1], [1, 2, 4, 6, 8], [1, 2, 10], [1, 2, 3, 4]]
 max = len(M[0])
 index = 0
+
 for i in range(1, len(M)):
     if len(M[i]) > max:
         max = len(M[i])
