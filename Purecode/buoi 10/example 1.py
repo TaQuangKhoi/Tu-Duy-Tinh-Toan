a7a31abe88c3653bd34f5196aef91401333188b3
Tạ Quang Khôi <khoi0941@gmail.com>
2022-12-06 22:42:26 +0700

    save

--- Purecode/buoi 10/example 1.py
+++ Purecode/buoi 10/example 1.py
@@ -1,59 +1,33 @@
-# 2. Viết chương trình yêu cầu người dùng nhập a, b và c là độ dài 3 cạnh của một tam giác.
-# Xử lý ngoại lệ trong các tình huống sau:
-# Người dùng nhập a, b hoặc c không phải là kiểu số
-# Người dùng nhập giá trị 0 hoặc số âm cho a, b hoặc c
-# Người dùng nhập a, b, c dương nhưng không thể là cạnh của một tam giác
+import os
 
-class ValueNotZeroError(Exception):
-    """Not zero value"""
-    pass
+os.chdir('C:\\My_Repos\\GitHub\\TaQuangKhoi\\Tu-Duy-Tinh-Toan')
 
+path = "text.txt"
 
-class ValueSmallThanZeroError(Exception):
-    """Value small than zero"""
-    pass
+x = []
+
+f = open(path, "r")
+lines = f.readlines()
+print(len(lines))
 
 
-class NotTriangleError(Exception):
-    """Not triangle"""
-    pass
+def first():
+    print("===In ra 5 dòng đầu===")
+    for i in range(5):
+        print(lines[i])
 
 
-def main():
-    while True:
-        try:
-            a = input_int("Nhập a: ")
-            b = input_int("Nhập b: ")
-            c = input_int("Nhập c: ")
-            if check_triangle(a, b, c):
-                raise NotTriangleError
-            else:
-                print("Đây là 3 cạnh của tam giác")
-                break
-        except NotTriangleError:
-            print("Không phải là tam giác")
+def second():
+    print("In ra 5 dòng cuối")
+    for y in range(-5, 0):
+        print(lines[y])
 
 
-def input_int(prompt):
-    while True:
-        try:
-            x = int(input(prompt))
-            if x == 0:
-                raise ValueNotZeroError
-            if x < 0:
-                raise ValueSmallThanZeroError
-            return x
-        except ValueError:
-            print("Bạn phải nhập số nguyên")
-        except ValueNotZeroError:
-            print("Bạn phải nhập số khác 0")
-        except ValueSmallThanZeroError:
-            print("Bạn phải nhập số dương")
+if len(lines) < 5:
+    print("===Nhỏ hơn 5===")
+    for r in range(len(lines)):
+        print(lines[r])
+else:
+    first()
+    second()
 
-
-def check_triangle(a, b, c):
-    if a + b <= c or a + c <= b or b + c <= a:
-        return True
-
-
-main()
