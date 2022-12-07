2b16349ffc9b793ba4961928e6fc717c366a8972
Tạ Quang Khôi <khoi0941@gmail.com>
2022-12-07 15:28:12 +0700

    update 'buoi 13.ipynb' and 'Learn Dictionary.py'

--- Purecode/buoi 13/Learn Dictionary.py
+++ Purecode/buoi 13/Learn Dictionary.py
@@ -1,5 +1,16 @@
-eng2vn = {}
-eng2vn['one'] = 'một'
-eng2vn['two'] = 'hai'
+dic = {}
+sohs = int(input("Nhập số hs:"))
+for i in range(sohs):
+    name = input("Nhập tên: ")
+    while name in dic:
+        print("Tên đã tồn tại")
+        name = input("Nhập tên khác: ")
+    
+    diem = input("Nhập điểm: ")
+    while diem.isdigit() == False:
+        print("Điểm không phù hợp")
+        diem =input("Nhập điểm: ")
+    
+    dic[name] = float(diem)
 
-print(eng2vn)
+dic
