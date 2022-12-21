a42be5682326dc096dd6c4562457111c50330358
TaQuangKhoi <khoi0941@gmail.com>
2022-12-21 15:43:21 +0700

    Lưu

--- Purecode/buoi 16/Bai 1.py
+++ Purecode/buoi 16/Bai 1.py
@@ -1,18 +1,47 @@
-def checkInt(so_nguyen):
-    if so_nguyen.isdigit():
-        return True
-    else: 
-        return False
-
-so_nguyen_1 = input("Nhập số nguyên 1: ")
-while(checkInt(so_nguyen_1) == False):
-    so_nguyen_1 = input("Nhập lại số nguyên 1: ")
 
 
-so_nguyen_2 = input("Nhập số nguyên 2: ")
-while(checkInt(so_nguyen_2) == False):
-    so_nguyen_2 = input("Nhập lại số nguyên 2: ")
+# def isfloat(num):
+#     try:
+#         float(num) # Check bằng ép kiểu
+#         return True
+#     except ValueError:
+#         return False
+
+# canh_a = input("Nhập cạnh a: ")
+# while(isfloat(canh_a) == False):
+#     canh_a = input("Nhập lại cạnh a: ")
+
+# canh_b = input("Nhập cạnh b: ")
+# while(isfloat(canh_b) == False):
+#     canh_b = input("Nhập lại cạnh a: ")
+
+# canh_c = input("Nhập cạnh c: ")
+# while(isfloat(canh_c) == False):
+#     canh_c = input("Nhập lại cạnh a: ")
+
+# def isTamGiac (a,b,c):
+#     return a+b>c and a+c>b and b+c>a
 
 
-tong = int(so_nguyen_1) + int(so_nguyen_2)
-print ("Tổng là", tong)
+
+import turtle
+import math
+
+pen = turtle.Pen()
+
+mau = ['red', 'blue', 'green']
+
+def veTamGiac(_a, _b, _c):
+    pen.color(mau[0])
+    pen.fd(_a)
+    pen.left(90)
+    cosa = (_a**2+_c**2-_b**2)/2*_a*_b
+    print(cosa)
+    print(math.cos(math.pi/cosa))
+
+
+veTamGiac(2, 3, 4)
+
+# if(isTamGiac(canh_a,canh_b,canh_c)):
+#     print("Bắt đầu vẽ tam giác")
+#     veTamGiac(20, 30, 40)
